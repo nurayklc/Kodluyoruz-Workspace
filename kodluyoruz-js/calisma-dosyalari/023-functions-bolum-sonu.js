fdae44b70f67dc60c41d8d99fa56458fde7daa70
nurayklc <nuriskl72@gmail.com>
2021-11-05 22:20:09 +0300

    Complete javascript functions.

---
+++ kodluyoruz-js/calisma-dosyalari/023-functions-bolum-sonu.js
@@ -0,0 +1,13 @@
+let counter = 0
+let counterDOM = document.querySelector("#counter")
+let increaseDOM = document.querySelector("#increase")
+let decreaseDOM = document.querySelector("#decrease")
+
+increaseDOM.addEventListener("click", clickEvent)
+
+decreaseDOM.addEventListener("click", clickEvent)
+
+function clickEvent() {
+    this.id == "increase" ? counter++ : counter--
+    counterDOM.innerHTML = counter
+}
