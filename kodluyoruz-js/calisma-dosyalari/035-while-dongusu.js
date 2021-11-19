a28ec8004bd4644c7c859cd5e570f953e071d0da
nurayklc <nuriskl72@gmail.com>
2021-11-19 22:42:25 +0300

    Start loop in js

---
+++ kodluyoruz-js/calisma-dosyalari/035-while-dongusu.js
@@ -0,0 +1,15 @@
+// While Dongusu
+
+// let counter = 0;
+
+// while ( counter < 10 ) {
+//   console.log(counter)
+//   counter ++
+// }
+
+let userName = ""
+
+while (!userName) {
+  userName = prompt("Kullanici Adini Giriniz: ")
+  console.log(userName)
+}
